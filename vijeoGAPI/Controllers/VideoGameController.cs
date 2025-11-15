d5d3af4885903dec21c54055e62137d0b9573102
Muhammad Abdul Moiz <moiza354@gmail.com>
2025-11-15 14:36:44 +0500

    model + controller + getRequest

--- vijeoGAPI/Controllers/VideoGameController.cs
+++ vijeoGAPI/Controllers/VideoGameController.cs
@@ -7,5 +7,32 @@ namespace vijeoGAPI.Controllers
     [ApiController]
     public class VideoGameController : ControllerBase
     {
+        public static List<VideoGame> videoGames = new List<VideoGame>
+        {
+            new VideoGame
+            {
+                Id = 1,
+                Title = "The Legend of Zelda: Breath of the Wild",
+                Platform = "Nintendo Switch",
+                Developer = "Nintendo EPD",
+                Publisher = "Nintendo",
+            },
+            new VideoGame
+            {
+                Id = 2,
+                Title = "God of War",
+                Platform = "PlayStation 4",
+                Developer = "Santa Monica Studio",
+                Publisher = "Sony Interactive Entertainment",
+            },
+            new VideoGame
+            {
+                Id = 3,
+                Title = "Red Dead Redemption 2",
+                Platform = "PS 4",
+                Developer = "Rockstar Studios",
+                Publisher = "Rockstar Games",
+            },
+        };
     }
 }
