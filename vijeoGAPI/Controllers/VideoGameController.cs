7d7557f56de6fa707de19483ff9a3c59dc2e851d
Muhammad Abdul Moiz <moiza354@gmail.com>
2025-11-15 14:23:27 +0500

    setup scalar

---
+++ vijeoGAPI/Controllers/VideoGameController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace vijeoGAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoGameController : ControllerBase
+    {
+    }
+}
